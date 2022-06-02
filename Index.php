a44a9bc0b188f9c04aab5d244963f2922bca7219
icpereiradoprado <icpereiradoprado@gmail.com>
2022-06-01 22:45:44 -0300

    Estilização dos cards

--- Index.php
+++ Index.php
@@ -9,6 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="wwwroot/css/CadImovel.css">
     <link rel="stylesheet" href="wwwroot/css/CadUser.css">
     <link rel="stylesheet" href="wwwroot/css/ListUsuario.css">
--- View/PartialsViews/CardImoveis.php
+++ View/PartialsViews/CardImoveis.php
@@ -1,38 +1,29 @@
 <?php
 require_once 'Controller/ImovelController.php';
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-</head>
-<body>
     <div class="container">
-        <div class="row">
-            
-            <?php 
-            $imoveis = call_user_func(array('ImovelController','listar'));
-            foreach($imoveis as $imovel)
-            {
-            ?>
+        <div class="row d-flex">    
+                <?php 
+                $imoveis = call_user_func(array('ImovelController','listar'));
+                foreach($imoveis as $imovel)
+                {
+                ?>
                 <div class="col-sm-4">
-            
-                    <div class="card mt-5 mb-5 text-center" style="width: 18rem; height: 350px;">
-                        <img src="..." class="card-img-top bg-img-gray" alt="...">
+                    <div class="card mt-5 mb-5 text-center card-size">
+                        <img src="<?php echo $imovel->getFoto()?>" class="card-img-top bg-img-gray" alt="">
                         <div class="card-body">
-                            <h5 class="card-title">Teste</h5>
-                            <p class="card-text text-start "><?php echo $imovel->getDescricao(); ?></p>
-                            <a href="#" class="btn btn-primary align-text-bottom"><?php echo $imovel->getTipo()?></a>
+                            <h5 class="card-title">Imóvel</h5>
+                            <p class="card-text text-start"><?php echo $imovel->getDescricao(); ?></p>
+                            
+                        </div>
+                        <div class="mb-3">
+                            <a href="#" class="btn btn-primary"><?php echo $imovel->getTipo()?></a>
                         </div>
                     </div>
                 </div>
-            <?php
-            }
-            ?>
+                <?php
+                }
+                ?>
         </div>
     </div>
-</body>
-</html>
